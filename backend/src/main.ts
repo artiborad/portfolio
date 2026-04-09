400f7665c8c0acbc96c5be0602eada9e55fcdbde
Arti Borad <artiborad0812@gmail.com>
2026-04-09 14:42:48 +0530

    Update CORS configuration and .env.example to include additional origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://portfolio-icug.onrender.com','https://portfolio-hazel-gamma-iys8i2z4ft.vercel.app'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
